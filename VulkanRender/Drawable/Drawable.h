ef2bd64e0e484c6a803464162fae7aac538084d7
SETSVPC <Hatsune_Miku_3939@outlook.com>
2025-06-28 20:30:31 +0800

    简单实现实例化渲染

---
+++ VulkanRender/Drawable/Drawable.h
@@ -0,0 +1,9 @@
+﻿#pragma once
+
+struct Drawable
+{
+    virtual void Renderer(const vk::CommandBuffer&, uint32_t) const = 0;
+    virtual void Update() = 0;
+
+    virtual ~Drawable() = default;
+};
